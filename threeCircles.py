2754cc9ee07a3ceec3c627305ffe75592331e22d
KristalMDow <30303538+KristalMDow@users.noreply.github.com>
2019-11-12 18:00:10 -0800

    Updated assignment to fit required parameters

--- threeCircles.py
+++ threeCircles.py
@@ -2,24 +2,18 @@
 # Using a function to create three growing circles.
 # By: Kristal Dow 11/12/19
 
-def drawCircle(x, y, size):
-    #create the Window object
-    win = GraphWin("Testing", 800, 800)
-    win.setBackground("Light Blue")
-
-    for i in range(1, 4):
-        # scale size and position of circles
-        x = i*x
-        y = i*y
-        size = i*size
-
-        #outputCircle
-        outputCircle = Circle(Point(x, y), size)
-        outputCircle.setOutline("White")
-        outputCircle.draw(win)
-
 #importing graphics library
 from graphics import *
 
-#call function to draw circle
-drawCircle(100, 100, 30)
+def drawFace(center, size, window):
+    head = Circle(center, size)
+    head.setFill("yellow")
+    head.draw(window)
+
+win = GraphWin("Faces")
+drawFace(Point(50,50), 10, win)
+drawFace(Point(100,100), 20, win)
+drawFace(Point(150,150), 30, win)
+win.getMouse()
+win.close()
+
